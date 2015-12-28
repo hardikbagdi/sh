9cd4699e24bd3b945298b5aac449ccc64246f0bd
Hardik Bagdi <hbagdi1@binghamton.edu>
2015-12-28 18:35:39 -0500

    Initial Commit

---
+++ bingWallpaper.sh
@@ -0,0 +1,22 @@
+#author: Hardik Bagdi (hardikbagdi@gmail.com)
+
+#ping url
+requestURL="http://www.bing.com/HPImageArchive.aspx?&format=js&idx=0&mkt=en-US&n=1"
+echo "pinging $requestURL"
+#ping using curl
+response=$(curl $requestURL)
+
+echo "\nresponse is: $response"
+#perl regex match
+urlfield=$(echo $response |grep -oP "url\":\"[^\"]*" )
+echo "\nextracted filed: $urlfield"
+#
+finalBaseURL=$(echo "http://www.bing.com")$(echo $urlfield | cut -d "\"" -f3)
+echo "\nfinal url of the image: $finalBaseURL"
+#download to Pictures Directory
+wget $finalBaseURL -P $HOME/Pictures
+
+imageName=${finalBaseURL##*/}
+echo "\nname of the image: $imageName"
+
+gsettings set org.gnome.desktop.background picture-uri "file://$HOME/Pictures/$imageName"
