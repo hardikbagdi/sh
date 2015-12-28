89c1cb683b4ed3961c20545d2518170ecb071727
Hardik Bagdi <hbagdi1@binghamton.edu>
2015-12-28 18:41:24 -0500

    Logs hidden

--- bingWallpaper.sh
+++ bingWallpaper.sh
@@ -2,21 +2,23 @@
 
 #ping url
 requestURL="http://www.bing.com/HPImageArchive.aspx?&format=js&idx=0&mkt=en-US&n=1"
-echo "pinging $requestURL"
+#echo "pinging $requestURL"
 #ping using curl
-response=$(curl $requestURL)
+echo "Downloading Image of the Day"
+response=$(curl -s $requestURL)
 
-echo "\nresponse is: $response"
+#echo "\nresponse is: $response"
 #perl regex match
 urlfield=$(echo $response |grep -oP "url\":\"[^\"]*" )
-echo "\nextracted filed: $urlfield"
+#echo "\nextracted filed: $urlfield"
 #
 finalBaseURL=$(echo "http://www.bing.com")$(echo $urlfield | cut -d "\"" -f3)
-echo "\nfinal url of the image: $finalBaseURL"
+#echo "\nfinal url of the image: $finalBaseURL"
 #download to Pictures Directory
-wget $finalBaseURL -P $HOME/Pictures
+wget -q $finalBaseURL -P $HOME/Pictures
 
 imageName=${finalBaseURL##*/}
-echo "\nname of the image: $imageName"
+#echo "\nname of the image: $imageName"
 
 gsettings set org.gnome.desktop.background picture-uri "file://$HOME/Pictures/$imageName"
+echo "Wallpaper Updated"
